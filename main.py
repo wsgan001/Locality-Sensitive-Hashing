e6bd5e97e248566c2346c3e2600b8d0b9545276c
Sevak Mardirosian <smardirosians@gmail.com>
2017-10-17 17:36:17 +0200

    pep8

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import numpy as np
 import sys
 import itertools
 
-seed = sys.argv[1]
-route = sys.argv[2]
-
-np.random.seed(seed=int(seed))
-input_data = np.load(rute)
-
 
 def min_hashing():
     return None
@@ -25,19 +19,25 @@ def output(results):
     for i in range(len(results)):
         results[i] = list(set(results[i]))
         for j in results:
-            col1 = matrix[:,i].nonzero()
+            col1 = matrix[:, i].nonzero()
             col2 = mtrix[:, j].nonzero()
             intersect = len(np.intersect1d(col1, col2))
             total = intersect + len(np.setxor1d(col1, col2))
             sim = float(intersect) / total
 
             if sim > 0.5:
-                count+=1
-                file.write(str(i+1) + ", " + str(j+1) + "\n")
+                count += 1
+                file.write(str(i + 1) + ", " + str(j + 1) + "\n")
     file.close()
 
+
 def main():
-    pass
+    seed = sys.argv[1]
+    route = sys.argv[2]
+
+    np.random.seed(seed=int(seed))
+    input_data = np.load(route)
+
 
 if __name__ == '__main__':
     main()
